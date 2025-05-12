4f36cf835cfdedbc4e4fe8a0ca7b71e89aa91948
김윤우 <gimyunu471@gmail.com>
2025-05-12 21:21:08 +0900

    enterandout

--- submit.c
+++ submit.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 
 void func3() {
-    
+    printf("→ Entering func3\n");
+    printf("← Exiting func3\n");
 }
 
 void func2() {
-    func3();  
+    printf("→ Entering func2\n");
+    func3();
+    printf("← Exiting func2\n");
 }
 
 void func1() {
-    func2();  
+    printf("→ Entering func1\n");
+    func2();
+    printf("← Exiting func1\n");
 }
 
 int main() {
-    func1();  
+    printf("→ Entering main\n");
+    func1();
+    printf("← Exiting main\n");
     return 0;
 }
